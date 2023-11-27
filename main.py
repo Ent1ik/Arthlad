020795d9f2e9a0a4daf49173c52462e64fcd960b
Ent1ik <152129046+Ent1ik@users.noreply.github.com>
2023-11-28 02:00:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(name)
 
-@app.route('/alice', methods=['POST'])
-def resp():
-    request_data = request.json.get('request', {})
-    command = request_data.get('command', '')
+@app.route('/', methods=['POST'])
+def webhook():
+    try:
+        data = request.json
+        text = data.get('request', {}).get('command')
+        response_text = "Assalamu aleykum"
 
-    response_text = "Assalamu aleykum"
+        response = {
+            'response': {
+                'text': response_text,
+                'end_session': False
+            },
+            'version': '1.0'
+        }
 
-    response = {
-        'response': {
-            'text': response_text,
-            'end_session': False
-        },
-        'session': request_data.get('session', {}),
-        'version': '1.0'
-    }
+Send response to the provided webhook
+        webhook_url = "https://hkdk.events/656RgpXWrrjC"
+        requests.post(webhook_url, json=response)
 
-    return jsonify(response)
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if name == 'main':
     app.run(debug=True)
