48156c00054c107476dbc6ff8a133838e517c77f
Ent1ik <152129046+Ent1ik@users.noreply.github.com>
2023-11-28 01:52:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from flask import Flask, request
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/alice', methods = ['POST'])
 def resp():
     text = request.json.get('request', {}).get('command')
-    response_text = f"Assalamu aleykum"
+    response_text = f"Привет, {text}! Как дела?"
     response = {
         'response' : {
             'text' : response_text,
-            'end_session' : False,
-            'intent' : 'response'
+            'end_session' : False
         },
-        'version' : '1.0'
+        'version' : '2.0'
     }
     return response
